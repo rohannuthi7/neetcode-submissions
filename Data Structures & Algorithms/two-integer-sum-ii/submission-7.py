80cbf6a203ea30625a7cd1845496193ec072e644
Rohan Nuthi <rohannuthi.rn@gmail.com>
2026-08-24 15:15:22 -0500

    Add: two-integer-sum-ii - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # pointers at opposite end of the number - if start + end is less, decrement the top pointer, if more, increment the bottom pointer - continue while the end >= start
+
+        start = 0
+        end = len(numbers) - 1
+        while numbers[start] + numbers[end] != target:
+            while numbers[start] + numbers[end] < target:
+                start += 1
+            while numbers[start] + numbers[end] > target:
+                end -= 1
+        
+        return [start + 1, end + 1]
